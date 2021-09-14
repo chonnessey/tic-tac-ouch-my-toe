0384e8b91404abc018653cc9834433c4d57c0e1a
Andrew Giang <giang184@gmail.com>
2021-09-14 11:04:41 -0700

    add state to Square
    Co-authored-by: dqveha <dqveha@users.noreply.github.com>
    Co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    }
+  }
   render() {
+    const test = this.props.value;
     return (
-      <button className="square">
+      <button className="square" onClick={() => {console.log(test); }}>
         {this.props.value}
       </button>
     );
