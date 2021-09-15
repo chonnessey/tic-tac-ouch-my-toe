8ed7cdfe06100981f0d64edab03d713e9c9e8e49
Andrew Giang <giang184@gmail.com>
2021-09-15 14:38:18 -0700

    update to use redux
    Co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>
    Co-authored-by: dqveha <dqveha@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App'
+import App from './components/App';
 import { createStore } from 'redux';
-import rootReducer from './reducers/index'
+import rootReducer from './reducers/index';
+import { Provider } from 'react-redux';
 
+const store = createStore(rootReducer);
 
-
-const store = createStore(rootReducer)
+store.subscribe(() =>
+  console.log(store.getState())
+);
 
 ReactDOM.render(
-  <React.StrictMode>  
+  <Provider store={store}>  
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
+
+
 // function calculateWinner(squares) {
 //   const lines = [
 //     [0, 1, 2],
