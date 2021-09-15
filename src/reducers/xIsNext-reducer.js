be54e8c2984adb47a326a8ee2293ec469149a365
Andrew Giang <giang184@gmail.com>
2021-09-15 15:19:08 -0700

    update xIsNext reducer
    Co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>
    Co-authored-by: dqveha <dqveha@users.noreply.github.com>

--- src/reducers/xIsNext-reducer.js
+++ src/reducers/xIsNext-reducer.js
@@ -1,7 +1,8 @@
 export default (state = true, action) => {
+  const { step } = action
   switch (action.type) {
     case 'TOGGLE_TURN':
-      return !state;
+      return (step % 2) === 0;
     default:
       return state;
   }
