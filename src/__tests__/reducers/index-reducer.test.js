2fd7e1a42e1be64103328e065c9632426ec90632
Andrew Giang <giang184@gmail.com>
2021-09-15 13:28:34 -0700

    pass fourth test for root reducer
    Co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>
    Co-authored-by: dqveha <dqveha@users.noreply.github.com>

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -24,8 +24,8 @@ describe("rootReducer", () => {
     expect(store.getState().history).toEqual(historyReducer([], { type: null }));
   });
   
-  // test('Check that initial state of formVisibleReducer matches root reducer', () => {
-  //   expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
-  // });
+  test('Check that initial state of stepNumberReducer matches root reducer', () => {
+    expect(store.getState().stepNumber).toEqual(stepNumberReducer(0, { type: null }));
+  });
 
 });
