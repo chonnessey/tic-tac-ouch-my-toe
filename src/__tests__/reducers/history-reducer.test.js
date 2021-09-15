ae72be938158d71687d4775d03342f2bdb0bffb2
Andrew Giang <giang184@gmail.com>
2021-09-15 11:24:05 -0700

    add 2nd test for history reducer
    Co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>
    Co-authored-by: dqveha <dqveha@users.noreply.github.com>

--- src/__tests__/reducers/history-reducer.test.js
+++ src/__tests__/reducers/history-reducer.test.js
@@ -1,11 +1,21 @@
 import historyReducer from '../../reducers/history-reducer';
 
 describe("historyReducer", () => {
+  let action;
+
   test('Should return default state if no action type is recognized', () => {
     expect(historyReducer([], {type: null})).toEqual([]);
   });
   
-  // test('Should return state + 1 with ADD_STEP action', () => {
-  //   expect(stepNumberReducer(0, {type: 'ADD_STEP'})).toEqual(1);
-  // });
+  test('Should return a concatanated copy of state with ADD_MOVE action', () => {
+    action = {
+      type: 'ADD_MOVE',
+      squares: [1],
+      positions: [1],
+    };
+    expect(historyReducer([], action)).toEqual([{
+      squares: [1],
+      positions: [1],
+    }]);
+  });
 });
